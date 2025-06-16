c1376ef2164e5a7237fc597104134c839bd463b4
Slayer13pro <gabriel_supremuz@hotmail.com>
2025-06-15 22:58:47 -0500

    Producto
    Listar, eliminar. Update parcial.

--- src/app/componente/producto-listar-component/producto-listar-component.ts
+++ src/app/componente/producto-listar-component/producto-listar-component.ts
@@ -1,11 +1,72 @@
-import { Component } from '@angular/core';
+import {Component, inject, ViewChild} from '@angular/core';
+import {
+  MatCell,
+  MatCellDef,
+  MatColumnDef,
+  MatHeaderCell,
+  MatHeaderCellDef, MatHeaderRow,
+  MatHeaderRowDef, MatRow, MatRowDef,
+  MatTable, MatTableDataSource
+} from '@angular/material/table';
+import {MatPaginator} from '@angular/material/paginator';
+import {MatSort, MatSortHeader} from '@angular/material/sort';
+import {DatePipe} from '@angular/common';
+import {MatButton} from '@angular/material/button';
+import {Router, RouterLink} from '@angular/router';
+import {Producto} from '../../model/producto';
+import {ProductoService} from '../../services/producto-service';
 
 @Component({
   selector: 'app-producto-listar-component',
-  imports: [],
+  imports: [
+    MatTable,
+    MatCellDef,
+    MatHeaderCellDef,
+    MatHeaderCell,
+    MatCell,
+    MatColumnDef,
+    MatPaginator,
+    MatHeaderRowDef,
+    MatRowDef,
+    MatSort,
+    MatHeaderRow,
+    MatRow,
+    DatePipe,
+    MatSortHeader,
+    MatButton,
+    RouterLink
+  ],
   templateUrl: './producto-listar-component.html',
   styleUrl: './producto-listar-component.css'
 })
 export class ProductoListarComponent {
 
+  lista: Producto[]=[];
+  displayedColumns: string[]=['idProducto', 'nombre', 'descripcion', 'precio', 'stock', 'id_categoria', 'Accion01', 'Accion02'];
+  dataSource: MatTableDataSource<Producto> = new MatTableDataSource<Producto>();
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+  @ViewChild(MatSort) sort: MatSort;
+  productoService: ProductoService = inject(ProductoService);
+  route:Router = inject(Router);
+  constructor() {
+    console.log("Constructor")
+  }
+  ngAfterViewInit() {
+    this.dataSource.sort = this.sort;
+    this.dataSource.paginator = this.paginator;
+  }
+  ngOnInit() {
+    this.productoService.list().subscribe({next: data => this.dataSource.data = data,
+      error: error => console.log(error)});
+  }
+
+  eliminar(id: number) {
+    this.productoService.delete(id).subscribe({
+      next: () => {
+        this.productoService.actualizarLista();
+      },
+
+    });
+  }
 }
+
--- src/app/componente/producto-nuevo-edit-component/producto-nuevo-edit-component.ts
+++ src/app/componente/producto-nuevo-edit-component/producto-nuevo-edit-component.ts
@@ -1,11 +1,110 @@
-import { Component } from '@angular/core';
+import {Component, inject} from '@angular/core';
+import {FormBuilder, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
+import {MatCard, MatCardContent, MatCardTitle} from '@angular/material/card';
+import {MatFormField, MatHint, MatInput, MatInputModule, MatLabel} from '@angular/material/input';
+import {
+  MatDatepicker,
+  MatDatepickerInput,
+  MatDatepickerModule,
+  MatDatepickerToggle
+} from '@angular/material/datepicker';
+import {MatButton} from '@angular/material/button';
+import {MatNativeDateModule, MatOption} from '@angular/material/core';
+import {ProductoService} from '../../services/producto-service';
+import {ActivatedRoute, Router} from '@angular/router';
+import {Producto} from '../../model/producto';
 
 @Component({
   selector: 'app-producto-nuevo-edit-component',
-  imports: [],
+  imports: [
+    MatCard,
+    MatCardTitle,
+    MatCardContent,
+    ReactiveFormsModule,
+    MatFormField,
+    MatLabel,
+    MatInput,
+    MatFormField,
+    MatDatepickerToggle,
+    MatDatepicker,
+    MatDatepickerInput,
+    MatButton,
+    MatHint,//add
+    MatInputModule,//add
+    MatDatepickerModule, // add
+    MatNativeDateModule,
+    MatOption,
+    // add
+  ],
   templateUrl: './producto-nuevo-edit-component.html',
   styleUrl: './producto-nuevo-edit-component.css'
 })
 export class ProductoNuevoEditComponent {
+  productoForm : FormGroup;
+  fb : FormBuilder = inject(FormBuilder)
+  productoService = inject(ProductoService);
+  router = inject(Router);
+  edicion : boolean = false;
+  route : ActivatedRoute = inject(ActivatedRoute);
+  id: number = 0;
 
+  constructor() {
+    this.productoForm = this.fb.group({
+      idProducto: [''],
+      nombre : ['', Validators.required],
+      descripcion : ['', Validators.required],
+      precio: ['', Validators.required],
+      stock: ['', Validators.required],
+      id_categoria : ['', [Validators.required]],
+    });
+  }
+
+  ngOnInit() {
+    this.route.params.subscribe(data => {
+      this.id = data['id'];
+      this.edicion = data['id'] != null;
+      this.cargaForm();
+    });
+
+  }
+
+  cargaForm(){
+    if(this.edicion){
+      this.productoService.listId(this.id).subscribe((data:Producto) => {
+        console.log(data);
+        this.productoForm.patchValue({
+          nombre:data.nombre,
+          descripcion:data.descripcion,
+          precio:data.precio,
+          stock:data.stock,
+          id_categoria:data.id_categoria
+        });
+      })
+    }
+  }
+
+  onSubmit(){
+    if(this.productoForm.valid){
+      const producto : Producto = new Producto();
+      producto.idProducto = this.id;
+      producto.nombre = this.productoForm.value.nombre;
+      producto.precio = this.productoForm.value.precio;
+      producto.descripcion = this.productoForm.value.descripcion;
+      producto.stock = this.productoForm.value.stock;
+      producto.id_categoria = this.productoForm.value.id_categoria;
+      if(!this.edicion){
+        console.log("Datos leidos del form:", producto);
+        this.productoService.insert(producto).subscribe((data) => {
+          console.log(data);
+          this.productoService.actualizarLista();
+          console.log("Lista Actualizada");
+        });
+      }
+      this.router.navigate(['productos']);
+    }
+    else{
+      console.log("Formulario no valido");
+    }
+  }
 }
+
