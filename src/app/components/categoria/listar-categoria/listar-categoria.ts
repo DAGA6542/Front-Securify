2e556838c90538218708bf7ba5f7d63200bea507
DAGA6542 <u202120374@upc.edu.pe>
2025-06-24 21:27:07 -0500

    .

--- src/app/components/categoria/listar-categoria/listar-categoria.ts
+++ src/app/components/categoria/listar-categoria/listar-categoria.ts
@@ -1,9 +1,27 @@
 import { Component } from '@angular/core';
+import {DatePipe} from '@angular/common';
+import {RouterLink} from '@angular/router';
 
 @Component({
   selector: 'app-listar-categoria',
   imports: [
-    
+    MatTable,
+    MatColumnDef,
+    MatHeaderRowDef,
+    MatRowDef,
+    DatePipe,
+    MatSort,
+    MatSortHeader,
+    MatButton,
+    RouterLink,
+    MatCellDef,
+    MatHeaderCellDef,
+    MatHeaderCell,
+    MatCell,
+    MatColumnDef,
+    MatHeaderRow,
+    MatRow,
+    MatPaginator
   ],
   templateUrl: './listar-categoria.html',
   styleUrl: './listar-categoria.css'
