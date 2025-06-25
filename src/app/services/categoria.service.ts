ec53497f1a26c7c9e712f82015f1cfb7636d6164
DAGA6542 <u202120374@upc.edu.pe>
2025-06-24 21:10:30 -0500

    Update categoria.service.ts

--- src/app/services/categoria.service.ts
+++ src/app/services/categoria.service.ts
@@ -1,9 +1,50 @@
-import { Injectable } from '@angular/core';
+import {inject, Injectable} from '@angular/core';
+import {environment} from '../../environments/environment';
+import {HttpClient} from '@angular/common/http';
+import {Observable, Subject} from 'rxjs';
+import {Categoria} from '../model/categoria';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CategoriaService {
 
+  private url: string = environment.apiURL;
+  private httpClient: HttpClient = inject(HttpClient);
+  private listaCambio: Subject<Categoria[]> = new Subject<Categoria[]>();
+
   constructor() { }
+
+  list(): Observable<any>{
+    return this.httpClient.get<Categoria[]>(this.url + "categoria/listacategoria");
+  }
+
+  listId(id: number): Observable<any> {
+    console.log(this.url + "categoria/buscacategoria/"+ id);
+    return this.httpClient.get<Categoria>(this.url+"categoria/buscacategoria/"+id);
+  }
+  insert(categoria:Categoria){
+    console.log(categoria);
+    return this.httpClient.post(this.url+"categoria/insertcategoria", categoria);
+  }
+
+  update(categoria: Categoria): Observable<any>{
+    return this.httpClient.put(this.url + "categoria/editcategoria", categoria);
+  }
+  delete(id: number): Observable<any>{
+    return this.httpClient.delete(this.url + "categoria/deletecat/" + id);
+  }
+  setList(listaNueva: Categoria[]) {
+    this.listaCambio.next(listaNueva); //envia la nueva lista a los suscriptores
+  }
+  getListaCambio(): Observable<Categoria[]>{
+    return this.listaCambio.asObservable();
+  }
+  actualizarLista(): void {
+    this.list().subscribe({
+      next: (data) => this.setList(data), //envia la nueva lista a los suscriptores
+      error: (err) => console.error('Error actualizando lista', err)
+    });
+  }
 }
+
