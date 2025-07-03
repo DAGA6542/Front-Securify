c539ef93a30112a0d69f81014489a5c72aeeaec0
DAGA6542 <u202120374@upc.edu.pe>
2025-07-03 01:54:07 -0500

    menu

--- src/app/components/home/home.ts
+++ src/app/components/home/home.ts
@@ -1,11 +1,43 @@
-import { Component } from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
+import {Router, RouterLink, RouterOutlet} from '@angular/router';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { CommonModule } from '@angular/common';
+import {MatSidenav, MatSidenavContainer, MatSidenavModule} from '@angular/material/sidenav';
+import {MatListItem, MatListModule, MatNavList} from '@angular/material/list';
+import {MatExpansionModule, MatExpansionPanel, MatExpansionPanelTitle} from '@angular/material/expansion';
+import {LoginService} from '../../services/login.service';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [
+    RouterOutlet,
+    MatToolbarModule,
+    RouterLink,
+    MatMenuModule,
+    MatIconModule,
+    MatButtonModule,
+    CommonModule,
+    RouterOutlet,
+    RouterLink,
+    MatToolbarModule,
+    MatMenuModule,
+    MatIconModule,
+    MatButtonModule,
+    CommonModule,
+    MatListModule,
+    MatSidenavModule,
+    MatExpansionModule
+  ],
   templateUrl: './home.html',
   styleUrl: './home.css'
 })
-export class Home {
+export class Home   {
+  title = 'Securify';
+  role: string = '';
+  loggedIn: boolean = false;
 
+  @ViewChild('sidenav') sidenav!: MatSidenav;
 }
--- src/app/model/registro.ts
+++ src/app/model/registro.ts
@@ -3,5 +3,4 @@ export class Registro {
   correo: String;
   password: String;
   confirmar: String;
-
 }
